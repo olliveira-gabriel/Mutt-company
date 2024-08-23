94b54f185529800f17745bebe52967ff8be1a87f
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-08-23 18:01:40 -0300

    Add files via upload

---
+++ MUTT/Tela-oakley/Tela-oakley.js
@@ -0,0 +1,10 @@
+function Logo_Header(){
+    // Redireciona o usuário para a página "Tela-princial.html"
+    window.location.href="../tela-principal/Tela-princial.html";
+}
+
+function Login_Header(){
+    // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
+    window.location.href="../Tela-login/Tela-Login.html";
+}
+
