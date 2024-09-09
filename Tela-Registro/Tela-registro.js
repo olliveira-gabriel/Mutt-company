e9d962450620ee89329f6d1b926f7c6f03d3fcd2
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-09-09 09:25:52 -0300

    Add files via upload

---
+++ Tela-Registro/Tela-registro.js
@@ -0,0 +1,9 @@
+function Logo_Header(){
+    // Redireciona o usuário para a página "Tela-princial.html"
+    window.location.href="../tela-principal/Tela-princial.html"
+}
+
+function  Registro_usuario(){
+    
+    window.location.href="../tela-principal/Tela-princial.html"
+}
