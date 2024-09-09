3d6f9881ec24f796525f0ea792050e1129636ea4
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-09-09 09:24:22 -0300

    Update Tela-login.js

--- Tela-login/Tela-login.js
+++ Tela-login/Tela-login.js
@@ -2,3 +2,7 @@ function Logo_Header(){
     // Redireciona o usuário para a página "Tela-princial.html"
     window.location.href="../tela-principal/Tela-princial.html"
 }
+function  Login_usuario(){
+    
+    window.location.href="../tela-principal/Tela-princial.html"
+}
