e34832f6746ce5fdf574f947feee4e4069f9ded6
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-09-25 12:23:52 -0300

    Delete Tela-login directory

--- Tela-login/Tela-login.js
+++
@@ -1,8 +0,0 @@
-function Logo_Header(){
-    // Redireciona o usuário para a página "Tela-princial.html"
-    window.location.href="../tela-principal/Tela-princial.html"
-}
-function  Login_usuario(){
-    
-    window.location.href="../tela-principal/Tela-princial.html"
-}
