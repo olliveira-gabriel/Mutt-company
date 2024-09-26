f9bd1c6ada2870fa97f7550236e55b369d1b71d6
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-09-26 12:33:03 -0300

    Update Tela-registro.js

--- Tela-Registro/Tela-registro.js
+++ Tela-Registro/Tela-registro.js
@@ -1,9 +1,41 @@
 function Logo_Header(){
     // Redireciona o usuário para a página "Tela-princial.html"
-    window.location.href="../tela-principal/Tela-princial.html"
+    window.location.href="../tela-principal/Tela-princial.html";
 }
 
 function  Registro_usuario(){
     
-    window.location.href="../tela-principal/Tela-princial.html"
+    let usuario =  document.getElementById('User').value;
+
+    let senha =  document.getElementById('Senha').value;
+
+    let confirmar_senha =  document.getElementById('Confirmar_senha').value;
+
+    let checkbox = document.getElementById('termos_de_contrato');
+
+    if(checkbox.checked){
+
+        if(senha === confirmar_senha){
+            let BancoDeDados = JSON.parse(localStorage.getItem("BancoDedados")) || {};
+            
+            if(BancoDeDados[usuario]){
+                
+                alert('Nome de usuário já cadastrado');
+                return;
+            }
+            BancoDeDados[usuario] = {
+                usuario: usuario,
+                password: senha
+            };
+            localStorage.setItem("BancoDeDados", JSON.stringify(BancoDeDados));
+            
+            alert('Usuário cadastrado com sucesso!');
+            window.location.href="../Tela-Login/Tela-Login.html";
+            
+        }else('As senhas são diferentes');
+        
+    }else{
+        alert('aceite os termos')
+    }
 }
+
