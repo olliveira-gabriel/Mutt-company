cf5d91d06f27b17e1f5e813a07f6de10cba57b64
Jeff himself <162985697+jeff0proprio@users.noreply.github.com>
2024-10-10 11:22:59 -0300

    Add files via upload

---
+++ Tela Login/Tela-login.js
@@ -0,0 +1,27 @@
+function Logo_Header(){
+    // Redireciona o usuário para a página "Tela-princial.html"
+    window.location.href="Tela-princial.html";
+}
+
+function Login_Header(){
+    // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
+    window.location.href="../Tela-Login/Tela-Login.html";
+}
+
+function logar(){
+    let usuario_login = document.getElementById('usuário_login').value;
+    let senha_login = document.getElementById('senha_login').value;
+    let BancoDeDados = JSON.parse(localStorage.getItem("bancoDeDados")) || {};
+
+    if(BancoDeDados[usuario_login] && BancoDeDados[usuario_login].password === senha_login){
+
+        window.location.href="Tela-princial.html";
+        
+    }else{
+            alert('Nome de usuário ou senha incorretos.')
+    }
+
+
+
+
+}
