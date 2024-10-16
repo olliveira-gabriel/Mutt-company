d57656070773a9ecc64c4409f65c75a9ef554e4a
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-10-16 17:34:00 -0300

    Add files via upload

---
+++ Tela-produtos-oakley/Oakley Teeth Blue Grey/Oakley Teeth Blue Grey.js
@@ -0,0 +1,33 @@
+function Logo_Header(){
+    // Redireciona o usuário para a página "Tela-princial.html"
+    window.location.href="../tela-principal/Tela-princial.html";
+}
+
+function Login_Header(){
+    // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
+    window.location.href="../Tela Login/Tela-Login.html";
+}
+
+function colocar_carrinho(nome, foto, preco){   
+    
+    let valorTamanho = document.querySelector("input[name='tamanho']:checked").value;
+
+    const produto = {
+        nome, 
+        foto,
+        preco,
+        valorTamanho
+    }
+
+   let carrinho = JSON.parse(localStorage.getItem('carrinho'))
+
+   if(carrinho){
+    carrinho.push(produto)
+   }else{
+    carrinho = [];
+    carrinho.push(produto)
+
+   }
+    localStorage.setItem('carrinho', JSON.stringify(carrinho))
+
+}
