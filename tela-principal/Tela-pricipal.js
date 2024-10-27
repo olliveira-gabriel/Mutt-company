f0de84400b30029a683da1305a64571ec53eb0eb
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-10-27 20:49:57 -0300

    Update Tela-pricipal.js

--- tela-principal/Tela-pricipal.js
+++ tela-principal/Tela-pricipal.js
@@ -8,8 +8,6 @@ function Login_Header(){
     window.location.href="../Tela Login/Tela-Login.html";
 }
 
-
-
 function Tela_oakley(){
     // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
     window.location.href="../Tela-oakley/Tela-oakley.html";
@@ -23,4 +21,24 @@ function Tela_nike(){
 function Tela_mizuno(){
     // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
     window.location.href="../Tela-mizuno/Tela-mizuno.html";
+}
+
+function Tela_tenis_oakley(){
+    window.location.href="../Tela-produtos-oakley/Oakley Teeth Blue Grey/Oakley-Teeth-Blue-Grey.html";
+
+}
+
+function Tela_jaqueta_oakley(){
+    window.location.href="../Tela-produtos-oakley/Oakley x Piet Jaqueta Static Anorak Olive/Oakley x Piet Jaqueta Static Anorak Olive.html";
+
+}
+
+function Tela_tenis_airmax95(){
+    window.location.href="../Tela-produtos-nike/nike_airmax95/nike_airmax95.html";
+
+}
+
+function Tela_tenis_tnpsg(){
+    window.location.href="../Tela-produtos-nike/nike_tn_psg/nike_tn_psg.html";
+
 }
