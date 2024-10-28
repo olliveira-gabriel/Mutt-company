40e14c928895efbd1ecac6e88b7895ecce0ca85f
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-10-28 09:51:11 -0300

    Update Tela-oakley.js

--- Tela-oakley/Tela-oakley.js
+++ Tela-oakley/Tela-oakley.js
@@ -26,3 +26,24 @@ function Tela_tenis_oakley(){
     window.location.href="../Tela-produtos-oakley/Oakley Teeth Blue Grey/Oakley-Teeth-Blue-Grey.html";
 
 }
+
+function Tela_oculos_oakley(){
+    window.location.href="../Tela-produtos-oakley/Piet x Oakley oculos Eye Jacket Orange/Piet-x-Oakley-oculos-Eye-Jacket-Orange.html";
+
+}
+
+function Tela_jaqueta_future_black(){
+    window.location.href="../Tela-produtos-oakley/Colete Oakley x Piet The Future Black/Colete Oakley x Piet The Future Black.html";
+
+}
+
+function Tela_jaqueta_Dog(){
+    window.location.href="../Tela-produtos-oakley/Oakley x Piet Moletom Monster Dog Solid Black/Oakley x Piet Moletom Monster Dog Solid Black.html";
+
+}
+
+function Tela_moletom_BigSkull(){
+    window.location.href="../Tela-produtos-oakley/Camiseta Oakley x Piet Big Skull LS Brown/Camiseta Oakley x Piet Big Skull LS Brown.html";
+
+}
+
