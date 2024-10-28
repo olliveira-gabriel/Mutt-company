ab81895f950bdcb4fa98370e646c4f8b599800dc
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-10-27 21:01:12 -0300

    Update Tela-oakley.js

--- Tela-oakley/Tela-oakley.js
+++ Tela-oakley/Tela-oakley.js
@@ -7,3 +7,22 @@ function Login_Header(){
     // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
     window.location.href="../Tela Login/Tela-Login.html";
 }
+function Logo_Header(){
+    // Redireciona o usuário para a página "Tela-princial.html"
+    window.location.href="../tela-principal/Tela-princial.html";
+}
+
+function Login_Header(){
+    // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
+    window.location.href="../Tela Login/Tela-Login.html";
+}
+
+function Tela_jaqueta_oakley(){
+    window.location.href="../Tela-produtos-oakley/Oakley x Piet Jaqueta Static Anorak Olive/Oakley x Piet Jaqueta Static Anorak Olive.html";
+
+}
+
+function Tela_tenis_oakley(){
+    window.location.href="../Tela-produtos-oakley/Oakley Teeth Blue Grey/Oakley-Teeth-Blue-Grey.html";
+
+}
