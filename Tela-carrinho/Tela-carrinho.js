e42895dd1e8a961613694d80c68c2739ad3f882d
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-10-31 12:11:26 -0300

    Update Tela-carrinho.js

--- Tela-carrinho/Tela-carrinho.js
+++ Tela-carrinho/Tela-carrinho.js
@@ -1,31 +1,28 @@
-const info_produtos = JSON.parse(localStorage.getItem('carrinho')) || []
+    const info_produtos = JSON.parse(localStorage.getItem('carrinho')) || []
 
-
-
-for(const i in info_produtos){
-    const produtos = info_produtos[i];
-    const li = document.createElement('li')
-    
-    const nome = info_produtos[i].nome;
-    
-    const tamanho = info_produtos[i].valorTamanho;
-    
-    const img = info_produtos[i].foto;
-    
-    const valor = info_produtos[i].preco;
     
     
-}  
+    
+    
+    
+    const nome = info_produtos[0].nome;
+    
+    const tamanho = info_produtos[0].valorTamanho;
+    
+    const img = info_produtos[0].foto;
+    
+    const valor = info_produtos[0].preco;
+    
+    
+    
+    document.getElementById("nome_produto").innerHTML = nome;    
+    
+    document.getElementById("tamanho_produto").innerHTML = (`Tamanho: ${tamanho}`);  
+    
+    document.getElementById("img_produto").src = img ;
+    
+    document.getElementById("img_produto").style.width = '100px';
+    
+    document.getElementById("Valor_produto").innerHTML = valor;
 
-        document.getElementById("nome_produto").innerHTML = nome;    
-
-        document.getElementById("tamanho_produto").innerHTML = (`Tamanho: ${tamanho}`);  
-
-        document.getElementById("img_produto").src = img ;
-
-        document.getElementById("img_produto").style.width = '100px';
-
-        document.getElementById("Valor_produto").innerHTML = valor;
-
-
-
+    
