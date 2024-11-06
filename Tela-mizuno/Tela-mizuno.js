5a02e9724dd8935aa14391fafc27b66543915e98
Bernardo Pozzebon Poluceno <162700391+Bernardo-03P@users.noreply.github.com>
2024-11-06 11:09:52 -0300

    Update Tela-mizuno.js

--- Tela-mizuno/Tela-mizuno.js
+++ Tela-mizuno/Tela-mizuno.js
@@ -7,3 +7,27 @@ function Login_Header(){
     // Redireciona o usuário para a página "Tela-Login.html" localizada na pasta "Tela-login"
     window.location.href="../Tela Login/Tela-Login.html";
 }
+function S_blue(){
+
+    window.location.href="../tela-produtos-mizuno/Mizuno Wave Prophecy 11S Blue/Mizuno Wave Prophecy 11S Blue.html";
+}
+function LS_seset(){
+
+    window.location.href="../tela-produtos-mizuno/Mizuno Wave Prophecy LS Desert Risk Red Diva Blue/Mizuno Wave Prophecy LS Desert Risk Red Diva Blue.html";
+}
+function Future_LS(){
+
+    window.location.href="../tela-produtos-mizuno/Mizuno Wave Prophecy LS Future Garden Summer Sand/Mizuno Wave Prophecy LS Future Garden Summer Sand.html";
+}
+function cherry_miz(){
+
+    window.location.href="../tela-produtos-mizuno/GCDS x Mizuno Wave Prophecy 8 Cherry/GCDS x Mizuno Wave Prophecy 8 Cherry.html";
+}
+function wave_10(){
+
+    window.location.href="../tela-produtos-mizuno/Mizuno Wave Rider 10 Sport White Silver/GCDS x Mizuno Wave Prophecy 8 Cherry.html";
+}
+function Prophecy_gold(){
+
+    window.location.href="../tela-produtos-mizuno/Shinzo x Mizuno Wave Prophecy LS Gold/GCDS x Mizuno Wave Prophecy 8 Cherry.html";
+}
