264489565ff99b4973e9441191c69e98afe7c1dc
gabriel oliveira <158064140+olliveira-gabriel@users.noreply.github.com>
2024-11-10 20:05:25 -0300

    Update Tela-carrinho.js

--- Tela-carrinho/Tela-carrinho.js
+++ Tela-carrinho/Tela-carrinho.js
@@ -1,12 +1,12 @@
-   
-   const info_produtos = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+    const info_produtos = JSON.parse(localStorage.getItem('carrinho')) || [];
     const produtos_carrinhos = document.getElementById('lista');
     produtos_carrinhos.innerHTML = '';
     let total = 0
-       
+    
+    
+    
     function exibir_carrinho(){
-
-
         for (const i in info_produtos){
             const produto = info_produtos[i];
             console.log(produto);
@@ -27,11 +27,26 @@
             produtos_carrinhos.appendChild(li);
             produtos_carrinhos.appendChild(img);
             
+            
+            document.getElementById('subtotal').innerHTML = (`Subtotal: R$${total}`)
+            if (total > 750){
+                document.getElementById('desconto').innerHTML = (`Subtotal: 20%`)
+            }else{
+                document.getElementById('desconto').innerHTML = (`Subtotal: 0%`)
+            }
         }
         console.log(total)
+        const desconto = (total * 20) / 100
+        console.log(desconto.toFixed(2))
+        Valor_total = total - desconto.toFixed(2)
+        document.getElementById('valortotal').innerHTML = (`Valor Total: R$${Valor_total.toFixed(2)}`)
+
     }
-    
     exibir_carrinho()
+    
+
+        
+
 
 
 
