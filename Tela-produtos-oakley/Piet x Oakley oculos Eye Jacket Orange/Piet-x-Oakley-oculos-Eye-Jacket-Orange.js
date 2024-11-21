7e34ba30bdfb73339a916c47b50cb5c14defc218
Caiquesbx <162700771+Caiquesbx@users.noreply.github.com>
2024-11-21 11:09:56 -0300

    Piet-x-Oakley-oculos-Eye-Jacket-Orange.js

--- Tela-produtos-oakley/Piet x Oakley oculos Eye Jacket Orange/Piet-x-Oakley-oculos-Eye-Jacket-Orange.js
+++ Tela-produtos-oakley/Piet x Oakley oculos Eye Jacket Orange/Piet-x-Oakley-oculos-Eye-Jacket-Orange.js
@@ -31,6 +31,6 @@ function colocar_carrinho(nome, foto, preco){
    }
     localStorage.setItem('carrinho', JSON.stringify(carrinho))
 
-    window.location.href="../Tela-carrinho/Tela-carrinho.html"
+    window.location.href="../../Tela-carrinho/Tela-carrinho.html"
 
 }
